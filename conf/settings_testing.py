7d7ba2161e39c2e35ced3a1673f0dc526deaffbc
linuxspring <381263761@qq.com>
2018-09-14 09:18:33 +0800

    a

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': '172.16.3.214',                        # 你的数据库password
+        'PASSWORD': '@86Dj3Y(it',                        # 你的数据库password
         'HOST': '172.16.3.214',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
