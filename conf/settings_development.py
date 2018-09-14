9c9abdb6b6ea8a433f134405d5823b71f03927e3
linuxspring <381263761@qq.com>
2018-09-14 14:27:22 +0800

    a

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -21,8 +21,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'gzcss',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
-        'PASSWORD': 'oa.oracle',                        # 你的数据库password
-        'HOST': '172.16.3.184',                   # 开发的时候，使用localhost
+        'PASSWORD': '@86Dj3Y(it',                        # 你的数据库password
+        'HOST': '172.16.3.214',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
