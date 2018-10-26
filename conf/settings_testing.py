e971979873f5d3afcec7ecd09fd30399cf563633
linuxspring <381263761@qq.com>
2018-10-26 10:51:49 +0800

    v1.0

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -20,9 +20,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': 'gzcss',                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                            # 你的数据库user
-        'PASSWORD': '@86Dj3Y(it',                        # 你的数据库password
-        'HOST': '172.16.3.214',                   		   # 数据库HOST
-        'PORT': '3306',                        # 默认3306
+        'USER': 'blueking',  # 你的数据库user root
+        'PASSWORD': 'blueking',  # 你的数据库password @86Dj3Y(it
+        'HOST': '172.16.11.12',  # 数据库HOST
+        'PORT': '1521',  # 默认3306
     },
 }
